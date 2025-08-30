eaf6d9a3771574ccc4d142b80a19c724839f70b6
jerry <jerry@actpass.com>
2025-08-30 19:32:34 +0800

    save

---
+++ solidity/denglian/foundry/hello_foundry/script/MyToken.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {MyToken} from "../src/MyToken.sol";
+
+contract MyTokenScript is Script {
+    MyToken public myToken;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        string memory name = "MyToken";
+        string memory symbol = "MT";
+        myToken = new MyToken(name, symbol);
+
+        vm.stopBroadcast();
+    }
+}
